bcd5b9c64323bead3c0eb19f468d100528f55838
Sebastian Siddi <ssiddi@gmail.com>
2025-04-25 12:06:15 -0300

    modifico permisos build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # build.sh: Script para que Render ejecute en cada despliegue
 set -eo pipefail
 
+
 pip install -r requirements.txt          # Instala dependencias
 python manage.py migrate                # Aplica migraciones de la BD
 python manage.py collectstatic --noinput  # Recolecta archivos estáticos
