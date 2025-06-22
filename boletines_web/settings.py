0715fb1d5f9054e7db0b144b0670dd45def5c1c8
Sebastian Siddi <ssiddi@gmail.com>
2025-06-22 16:19:05 -0300

    agrego .com.ar al alowed hosts

--- boletines_web/settings.py
+++ boletines_web/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com','www.stepbystepbarracas.com.ar','.stepbystepbarracas.com.ar','stepbystepbarracas.com.ar']
 
 
 
